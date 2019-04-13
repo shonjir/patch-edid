908a5b486a8c262541cbd4a28a654beca4f80184
shonjir <shonjir@users.noreply.github.com>
2019-04-13 11:28:45 -0700

    show diff between EDID blocks; correct plist tabstops

--- patch-edid.rb
+++ patch-edid.rb
@@ -44,20 +44,20 @@ displays.each do |disp|
 
   # Show some info
   puts "Found display '#{monitor_name}': vendorid #{disp["vendorid"]}, productid #{disp["productid"]}"
-  puts "Original EDID:\n#{disp["edid_hex"]}"
   puts
   puts "EDID version #{bytes[18]}.#{bytes[19]}"
 
   puts "Features:"
-  digital_display = (bytes[20] & (0x80))>>7
-  color_format = (bytes[24] & (0b11000))>>3
-  digital_formats = ["RGB 4:4:4", "RGB 4:4:4 + YCrCb 4:4:4", "RGB 4:4:4 + YCrCb 4:2:2", "RGB 4:4:4 + YCrCb 4:4:4 + YCrCb 4:2:2"]
-  analog_formats = ["Monochrome or Grayscale", "RGB Color", "Non-RGB Color", "Undefined"]
-  if (digital_display)
-    puts "  Digital Display (#{digital_formats[color_format]})"
+  # Display type
+  if (bytes[20] & (0x80))>>7
+    display_type = "Digital"
+    formats = ["RGB 4:4:4", "RGB 4:4:4 + YCrCb 4:4:4", "RGB 4:4:4 + YCrCb 4:2:2", "RGB 4:4:4 + YCrCb 4:4:4 + YCrCb 4:2:2"]
   else
-    puts "  Analog Display (#{analog_formats[color_format]})"
+    display_type = "Analog"
+    formats = ["Monochrome or Grayscale", "RGB Color", "Non-RGB Color", "Undefined"]
   end
+  color_format = (bytes[24] & (0b11000))>>3
+  puts "  #{display_type} Display (#{formats[color_format]})"
   if bytes[24] & (0b10000000)
     puts "  DPMS standby"
   end
@@ -72,8 +72,12 @@ displays.each do |disp|
   end
   puts "Number of extension blocks: #{bytes[126]}"
 
-  puts "Setting color support to RGB 4:4:4 only"
+  puts
+  puts "Patching EDID..."
+  puts "  Setting color support to RGB 4:4:4 only"
   bytes[24] &= ~(0b11000)
+  new_color_format = (bytes[24] & (0b11000))>>3
+  puts "  New color format: #{formats[new_color_format]}"
 
   # Optional - remove extension block(s)
   #puts "removing extension block"
@@ -82,10 +86,23 @@ displays.each do |disp|
 
   # Recalculate EDID checksum
   bytes[127] = (0x100-(bytes[0..126].reduce(:+) % 256)) % 256
-  puts "Recalculated checksum: 0x%x" % bytes[127]
-  puts
-  puts "New EDID:\n#{bytes.map{|b|"%02X"%b}.join}"
+  puts "Recalculated checksum: 0x%02x" % bytes[127]
 
+  # Display old/new EDID blocks
+  new_edid = bytes.map{|b|"%02x"%b}.join
+  puts
+  puts "Original EDID:\n#{disp["edid_hex"]}"
+  puts
+  puts "New EDID:\n#{new_edid}"
+
+  def diff( s1, s2 )
+    t = s1.chars
+    return s2.chars.map{|c| c == t.shift ? '-' : c}.join
+  end
+  puts
+  puts "Difference:\n#{diff(disp["edid_hex"], new_edid)}"
+
+  $tab = "\t"
   def plist_key_value( sp, key, type, value )
     str =  "#{sp}<key>#{key.to_s}</key>\n"
     str += "#{sp}<#{type}>#{value.to_s}</#{type}>"
@@ -95,8 +112,8 @@ displays.each do |disp|
   # Write an edid patch stanza
   def plist_edid_patch( sp, offset, bytes )
     str = "#{sp}<dict>\n"
-    str += plist_key_value( sp + '  ', "offset", "integer", offset ) + "\n"
-    str += plist_key_value( sp + '  ', "data", "data", Base64.strict_encode64( bytes.pack('C*') ).to_s ) + "\n"
+    str += plist_key_value( sp + $tab, "offset", "integer", offset ) + "\n"
+    str += plist_key_value( sp + $tab, "data", "data", Base64.strict_encode64( bytes.pack('C*') ).to_s ) + "\n"
     str += "#{sp}</dict>"
     return str
   end
@@ -113,13 +130,13 @@ displays.each do |disp|
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-#{ plist_key_value( '  ', "DisplayVendorID", "integer", disp["vendorid"] ) }
-#{ plist_key_value( '  ', "DisplayProductID", "integer", disp["productid"] ) }
-#{ plist_key_value( '  ', "DisplayProductName", "string", "#{monitor_name} (RGB 4:4:4)" ) }
-  <key>edid-patches</key>
-    <array>
-#{ plist_edid_patch( '      ', 24, bytes[24..24] ) }
-    </array>
+#{ plist_key_value( $tab, "DisplayVendorID", "integer", disp["vendorid"] ) }
+#{ plist_key_value( $tab, "DisplayProductID", "integer", disp["productid"] ) }
+#{ plist_key_value( $tab, "DisplayProductName", "string", "#{monitor_name} (RGB 4:4:4)" ) }
+#{$tab}<key>edid-patches</key>
+#{$tab}<array>
+#{ plist_edid_patch( $tab + $tab, 24, bytes[24..24] ) }
+#{$tab}</array>
 </dict>
 </plist>
 PLIST
