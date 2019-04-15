1ce44bc55a236b70d457ca9a14390a7359244bf3
shonjir <shonjir@users.noreply.github.com>
2019-04-14 21:50:23 -0700

    add command parameter to link instead of copy

--- install-edid.sh
+++ install-edid.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Install display overrides to all mounted system volumes
 
+case "$1" in
+  link|test) LINK="ln -vsf" ;;
+  *)    COPY="cp -vf" ;;
+esac
+
 find /Volumes -type d -mindepth 1 -maxdepth 1 | while read mountpoint
 do
   echo "Checking ${mountpoint}..."
@@ -8,12 +13,12 @@ do
   overrides="${mountpoint}/System/Library/Displays/Contents/Resources/Overrides/"
   if [ -d "${overrides}" ]; then
     echo "Installing EDID overrides to ${overrides}..."
-    #cp -rv DisplayVendorID-* "${overrides}"
-    dir=$(pwd)
-    dir="${dir##${mountpoint}}"
+    src=$(pwd)
+    src="${src##${mountpoint}}"
     find DisplayVendorID-* -type f | while read line
     do
-      ln -vsf "${dir}/${line}" "${overrides}${line}"
+      mkdir -p "${overrides}${line%/*}"
+      ${COPY} "${src}/${line}" "${overrides}${line}"
     done
   else
     echo "${mountpoint}: Not a system volume!"
